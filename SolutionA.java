7740718b98d91c6d97ba73606166622227745d03
Amritansh <amritanshm21@gmail.com>
2019-03-07 03:56:13 +0530

    This is my First Commit(aiwehi kch v dal diya h yrr)

---
+++ SolutionA.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+  
+public class SolutionA{
+  public static void main(String[] args){
+    
+    Scanner input= new Scanner(System.in);
+    int n = input.nextInt();
+    int[] arr = new int[n+1];
+    
+    for(int i=1;i<=n;i++){
+      arr[i] = input.nextInt();
+    }
+    
+    int k=-1;
+    
+    for(int i=1;i<=n;i++){
+      if(arr[i] == i){
+        k=i;
+        break;
+      }
+    }
+     
+    if(k != -1){
+      System.out.println(k);
+    }
+    else{
+      System.out.println("NOT_FOUND");
+    }
+  }
+}
